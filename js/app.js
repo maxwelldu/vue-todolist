0becfaf892ac514e490c6ddd887e30a89938bc0f
maxwelldu <dcj3sjt@163.com>
2017-11-12 09:56:05 +0800

    搭建程序的框架结构

---
+++ js/app.js
@@ -0,0 +1,27 @@
+(function(){
+  // 实例化
+  var app = new Vue({
+    // 挂载元素
+    el: '.todoapp',
+    // 属性
+    data: {
+
+    },
+    // 计算属性
+    computed: {
+
+    },
+    // 属性观察
+    watch: {
+
+    },
+    // 方法集合
+    methods: {
+
+    },
+    // 指令集合
+    directive: {
+
+    },
+  })
+})();
